d8de557289859b4e38083ca939adb7860fbe4668
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-01-03 09:59:34 +0000

    fmt

--- src/lifetimes/mod.rs
+++ src/lifetimes/mod.rs
@@ -1,13 +1,13 @@
 // Lifetime practice modules
 
 // Basic lifetime operations
-pub mod longer_string;
-pub mod split_first_word;
-pub mod find_longest_string;
-pub mod repeat_longer_owned;
 pub mod after_char;
+pub mod find_longest_string;
+pub mod longer_string;
+pub mod repeat_longer_owned;
+pub mod split_first_word;
 
 // Advanced lifetime patterns
-pub mod text_holder;
 pub mod config_manager;
 pub mod lifetime_structs;
+pub mod text_holder;
