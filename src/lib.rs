ab0750d06308df99d5037255f2e4c52166b34325
Daniel Cox <63171098+dan-kc@users.noreply.github.com>
2026-07-25 14:22:55 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,12 @@
 )]
 #![feature(mpmc_channel)]
 
+pub mod axum;
+pub mod collections;
+pub mod concurrency;
 pub mod file_io;
 pub mod iterators;
 pub mod lifetimes;
 pub mod smart_pointers;
 pub mod string_manipulation;
-
-// New comprehensive practice modules
-pub mod axum;
-pub mod collections;
-pub mod concurrency;
 pub mod tokio;
